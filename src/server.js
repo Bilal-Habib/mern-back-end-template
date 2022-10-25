1bcdae1f24e8824545c798e63a756735208290c3
Bilal Habib <bilal.habib67@gmail.com>
2022-10-25 11:01:54 +0100

    added mongodb to server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { db } from "./db";
 import { routes } from "./routes";
 
 const app = express();
@@ -7,9 +8,14 @@ const app = express();
 app.use(bodyParser.json());
 
 routes.forEach((route) => {
-    app[route.method](route.path, route.handler);
+  app[route.method](route.path, route.handler);
 });
 
-app.listen(8080, () => {
+const start = async () => {
+  await db.connect("mongodb://localhost:27017");
+  app.listen(8080, () => {
     console.log("Server is listening on port 8080");
-});
+  });
+};
+
+start();
